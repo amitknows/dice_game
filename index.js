176fd016fccda168d2f3fcfc4ae5c200ea566b83
amitknows <114345326+amitknows@users.noreply.github.com>
2023-03-13 18:05:52 +0530

    Dice_1.0

---
+++ index.js
@@ -0,0 +1,25 @@
+var randomNumber_1 = (Math.floor(Math.random()*6))+1;
+var randomDiceImage = "images/dice"+randomNumber_1+".png";
+document.querySelectorAll("img")[0].setAttribute("src", randomDiceImage);
+
+
+
+
+var randomNumber_2 = (Math.floor(Math.random()*6))+1;
+var ran_img = "images/dice"+randomNumber_2+".png";
+document.querySelectorAll("img")[1].setAttribute("src", ran_img);
+
+
+
+
+if(randomNumber_1 === randomNumber_2){
+document.querySelector("h1").innerHTML = "Draw";
+}
+
+else if(randomNumber_1<randomNumber_2){
+    document.querySelector("h1").innerHTML = "Player 2 wins";
+}
+
+else{
+    document.querySelector("h1").innerHTML = "Player 1 wins";
+}
